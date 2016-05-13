e46cdcd80099b011e70cc7c6701bdd2f47cda808
Udi Finkelstein <github@udifink.com>
2016-05-13 10:19:24 +0300

    More C++-ism

--- dmpdiff/dmpdiff.cpp
+++ dmpdiff/dmpdiff.cpp
@@ -7,62 +7,45 @@
 //
 
 #include <iostream>
-extern "C" {
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-}
+#include <sstream>
+#include <fstream>
+#include <codecvt>
+#include <locale>
+
 #include "diff_match_patch.h"
 
+std::wstring readFile(const char* filename)
+{
+    std::wifstream wif(filename);
+    wif.imbue(std::locale::global(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>)));
+    std::wstringstream wss;
+    wss << wif.rdbuf();
+    return wss.str();
+}
 
-const int DMP_MAX_FILE_SIZE=100000000;
-
-enum class df {
-    DMP_GFS_OK = 0,
-    DMP_GFS_NO_FILE,
-    DMP_GFS_FILE_TOO_LARGE,
-    DMP_GFS_FILENAME_TOO_LONG,
-    DMP_GFS_MMAP_FAILED,
-    DMP_GFS_STAT_FAILED
-};
-
-class diff_file {
-    std::string str;
-    int fd;
-public:
-    ~diff_file() {
-        if (fd > 0) {
-            fd = 0;
-            close(fd);
-        }
+//std::ostream& operator<<(std::ostream& os, const Diff& d)
+static void dump_Diff(const Diff& d)
+{
+    switch(d.operation) {
+        case INSERT:
+            std::wcout << "+" << d.text;
+            break;
+        case DELETE:
+            std::wcout << "-" << d.text;
+            break;
+        case EQUAL:
+            std::wcout << "=" << d.text;
+            break;
     }
-    diff_file(const char *name) {
-        struct stat st;
-    
-        if (strnlen(name, FILENAME_MAX) == FILENAME_MAX) {
-            throw df::DMP_GFS_FILENAME_TOO_LONG;
-        }
-        fd = open(name, O_RDONLY);
-        if (fd < 0)
-            throw df::DMP_GFS_NO_FILE;
-        if (fstat(fd, &st) < 0)
-            throw df::DMP_GFS_STAT_FAILED;
-        if (st.st_size > DMP_MAX_FILE_SIZE) {
-            throw df::DMP_GFS_FILE_TOO_LARGE;
-        };
-        str.resize(st.st_size); // lseek(df->fd, (off_t)0, SEEK_END);
-        std::cout << "len:" << st.st_size << "\n";
-        if (read(fd, &str[0], st.st_size) < 0) {
-            std::cout << "read() failed: \n";
-            throw df::DMP_GFS_MMAP_FAILED;
-        }
-    }
-};
+}
 
+static void dump_diff(std::list<Diff> ld)
+{
+    for (auto diff : ld) {
+        dump_Diff(diff);
+    }
+}
 int main(int argc, const char * argv[]) {
-    int r1, r2;
 
     /*
      * Sanity checking
@@ -73,8 +56,11 @@ int main(int argc, const char * argv[]) {
     }
     try {
         diff_match_patch dmp;
-        diff_file f1(argv[1]), f2(argv[2]);
-        dmp.diff_main(&f1.str, &f2.str);
+        std::wstring wstr1 = readFile(argv[1]);
+        std::wstring wstr2 = readFile(argv[2]);
+        
+        std::list<Diff> result = dmp.diff_main(wstr1, wstr2);
+        dump_diff(result);
         //dmp_diff_print_raw(stdout, diff);
         //dmp_diff_free(diff);
     }
@@ -83,6 +69,8 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     return 0;
 }
+
+
 
 
 
